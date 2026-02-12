c8436042e535fe3dcb4983e44a2ac4e11a610534
Roberto Parra <uedsoldier1990@gmail.com>
2026-02-12 17:46:43 -0600

    Add app.js file with initial JavaScript code for logging and alerts

---
+++ dist/static/js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+console.log("Hola desde JS");
+alert("Hola desde JS");
+
+let i = 0;
+
+for (let i = 100; i < 1000; i++) {
+    console.log(`Contador: ${i}`);
+    String()   
+    
+}
+
+console.log(`Contador fuera ${i}`);
